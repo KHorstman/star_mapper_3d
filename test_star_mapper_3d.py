e8c2f69a9b027d4b8714d328ece58c4250007a1e
KHorstman <katelynhorstman@gmail.com>
2023-07-14 07:40:14 -0500

    finished test

--- test_star_mapper_3d.py
+++ test_star_mapper_3d.py
@@ -1,6 +1,15 @@
+from json.tool import main
 from main import make_star_box
 
 def test_star_mapper_3d():
-    make_star_box("HD 984", 1, 0, 100)
+    """
+    Test to make sure a plot is created every time the code is ran. 
+    """
+    star_name="HD 984"
+    radius=1
+    depth_start=0
+    depth_end=100
+
+    make_star_box(star_name, radius, depth_start, depth_end)
 
 test_star_mapper_3d()
